41d0c9a2911f31677d7eaff5258d8defde3d75e4
Lena Kritinina <k0209008@gmail.com>
2025-04-07 00:41:24 +0500

    add check and enum

--- cmx-cards-impl/src/main/java/kz/osu/cinimex/entity/Card.java
+++ cmx-cards-impl/src/main/java/kz/osu/cinimex/entity/Card.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -30,27 +31,26 @@ public class Card {
     private String cardholder;
 
     @Column(name = "open_date", nullable = false)
-    private Date openDate;
+    private LocalDate openDate;
 
     @Column(name = "end_date", nullable = false)
-    private Date endDate;
+    private LocalDate endDate;
 
     @Column(name = "close_date")
-    private Date closeDate;
+    private LocalDate closeDate;
 
     @Column(name = "is_blocked", nullable = false)
     private Boolean isBlocked;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "current_status", length = 10, nullable = false)
-    private String currentStatus;
+    private AccountStatus currentStatus;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "pay_system_code", nullable = false)
     private PaySystem paySystem;
 
-    @ManyToMany()
-    @JoinTable(name = "occ_account_card",
-               joinColumns = @JoinColumn(name = "card_id"),
-               inverseJoinColumns = @JoinColumn(name = "account_id"))
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
     private List<UUID> accounts = new ArrayList<>();
 }
