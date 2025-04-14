390c4f7c857337c41c50a20c67e6acf901a53c07
Lena Kritinina <k0209008@gmail.com>
2025-04-14 22:56:22 +0500

    fix api

--- cmx-users-api/src/main/java/kz/osu/cinimex/controller/UserController.java
+++ cmx-users-api/src/main/java/kz/osu/cinimex/controller/UserController.java
@@ -24,8 +24,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Optional;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping("api/users")
