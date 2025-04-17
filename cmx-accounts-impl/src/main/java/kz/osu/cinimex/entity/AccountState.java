6e3d8c716868dd76f6e030c7b4152443f05e6840
Lena Kritinina <k0209008@gmail.com>
2025-04-17 18:53:25 +0500

    18: altered oca_account, added client description

--- cmx-accounts-impl/src/main/java/kz/osu/cinimex/entity/AccountState.java
+++ cmx-accounts-impl/src/main/java/kz/osu/cinimex/entity/AccountState.java
@@ -1,14 +1,20 @@
 package kz.osu.cinimex.entity;
 
 public enum AccountState {
-    REQUESTED_BY_USER,
-    REQUEST_SENT_TO_CFT,
-    CHECKS_STARTED,
-    OPENED,
-    CHECKS_COMPLETED,
-    CHECKS_FAILED,
-    REQUEST_REJECTED,
-    REQUEST_REJECTED_BY_CFT,
-    BLOCKED,
-    CLOSED
+    REQUESTED_BY_USER("Запрошено открытие счета"),
+    REQUEST_SENT_TO_CFT("Внутренние проверки банк"),
+    CHECKS_STARTED("Внутренние проверки банка"),
+    OPENED("Счет открыт"),
+    CHECKS_COMPLETED("Проверки банка пройдены"),
+    CHECKS_FAILED("Проверки банка не пройдены, требуются ручные проверки"),
+    REQUEST_REJECTED("Открытие счета отменено"),
+    REQUEST_REJECTED_BY_CFT("Отказано в открытии счета"),
+    BLOCKED("Счет заблокирован"),
+    CLOSED("Счет закрыт");
+
+    public final String label;
+
+    private AccountState(String label) {
+        this.label = label;
+    }
 }
